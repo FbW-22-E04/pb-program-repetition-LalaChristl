dbd13c55b6aecd839d5963da57b9315a91d6ae4b
LalaChristl <lesleyannchristl@gmail.com>
2022-07-12 15:25:18 +0200

    added JS

---
+++ solutions.js
@@ -0,0 +1,26 @@
+let i = 0;
+
+while (i < 15) {
+    console.log(i);
+i++
+}
+
+console.log('----------------------------------------------');
+
+let x = 1;
+let sum = 0;
+while (x <= 20) {
+    sum = sum + x;
+    x++
+    console.log(sum);
+}
+
+console.log('----------------------------------------------');
+
+let y = 0;
+
+do {
+    console.log('The number is', y);
+    y++
+}
+while (y < 20)
